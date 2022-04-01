c012944d4fb9e1a3f027e10f78f930a3682c22c9
ysimonson <simonson@gmail.com>
2022-03-31 21:53:40 -0400

    Rename module

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,15 @@
+mod entry;
 mod errors;
 mod memory;
-mod models;
 mod traits;
 mod utils;
 
 #[cfg(feature = "sqlite")]
 mod sqlite;
 
+pub use self::entry::Entry;
 pub use self::errors::Error;
 pub use self::memory::MemoryStore;
-pub use self::models::Entry;
 pub use self::traits::{Range, Store};
 
 #[cfg(feature = "sqlite")]
