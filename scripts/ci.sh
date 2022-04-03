e60932236d0f64b3b629252b37c86fd6c11bdd2a
ysimonson <simonson@gmail.com>
2022-04-03 16:24:10 -0400

    Move testing out to a script

---
+++ scripts/ci.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -ex
+
+rust_variant=$1
+os=$2
+
+cargo test
+(cd fuzz && cargo check)
+
+if [ "$os" == "ubuntu-latest" ]; then
+    if [ "$rust_variant" == "stable" ]; then
+        cargo fmt -- --check
+    else
+        cargo check --all-features
+        cargo clippy --all-features
+    fi
+fi
+
+pip install virtualenv
+make venv
+source venv/bin/activate
+maturin develop --cargo-extra-args="--features=python"
+pytest --color=yes python_tests/
