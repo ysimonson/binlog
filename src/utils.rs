0a3bd21221afb2a86c2f7e943dc1dceb26cafbdd
Yusuf Simonson <simonson@gmail.com>
2022-04-03 21:27:23 -0400

    Fixed utils test

--- src/utils.rs
+++ src/utils.rs
@@ -19,15 +19,14 @@ pub(crate) fn check_bounds(start_bound: Bound<&i64>, end_bound: Bound<&i64>) -> 
 
 #[cfg(test)]
 mod tests {
-    use crate::Error;
     use std::ops::Bound;
 
     #[test]
     fn check_bounds() {
         super::check_bounds(Bound::Unbounded, Bound::Unbounded).unwrap();
-        super::check_bounds(Bound::Included(0), Bound::Unbounded).unwrap();
+        super::check_bounds(Bound::Included(&0), Bound::Unbounded).unwrap();
         // TODO: maybe this should return a BadRange?
-        super::check_bounds(Bound::Unbounded, Bound::Included(0)).unwrap();
-        super::check_bounds(Bound::Unbounded, Bound::Excluded(0)).unwrap();
+        super::check_bounds(Bound::Unbounded, Bound::Included(&0)).unwrap();
+        super::check_bounds(Bound::Unbounded, Bound::Excluded(&0)).unwrap();
     }
 }
