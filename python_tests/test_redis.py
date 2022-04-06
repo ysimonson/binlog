25548cb2d11b3d88e039a0135255e2efb7c46714
ysimonson <simonson@gmail.com>
2022-04-05 22:54:14 -0400

    Fixed pub/sub checking

--- python_tests/test_redis.py
+++ python_tests/test_redis.py
@@ -1,10 +1,18 @@
 import tempfile
+import time
 from binlog import binlog
 
 
-def test_push():
+def test_pubsub():
     store = binlog.RedisStreamStore("redis://localhost:6379", 10)
     entry = binlog.Entry(1, "pytest_redis_push", [1, 2, 3])
     sub = store.subscribe("pytest_redis_push")
+    time.sleep(0.1) # give enough time for the thread to start up
     store.push(entry)
-    assert next(sub) == entry
+    sub_entry = next(sub)
+    print(dir(entry))
+    print(dir(sub_entry))
+    assert entry.timestamp == sub_entry.timestamp
+    assert entry.name == sub_entry.name
+    assert entry.value == sub_entry.value
+
