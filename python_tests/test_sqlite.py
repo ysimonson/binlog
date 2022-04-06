55b7ee796112910894df6ec5d04f4850e40dcbf3
ysimonson <simonson@gmail.com>
2022-04-05 21:42:40 -0400

    Basic redis store in python

--- python_tests/test_sqlite.py
+++ python_tests/test_sqlite.py
@@ -5,4 +5,4 @@ from binlog import binlog
 def test_push():
     with tempfile.NamedTemporaryFile(suffix="binlog.db") as f:
         store = binlog.SqliteStore(f.name)
-        store.push(binlog.Entry(1, "pytest_push", [1, 2, 3]))
+        store.push(binlog.Entry(1, "pytest_sqlite_push", [1, 2, 3]))
