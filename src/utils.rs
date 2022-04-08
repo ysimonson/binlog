e1b9cf7e9ed59876e4d8b46beac163dd847e494a
ysimonson <simonson@gmail.com>
2022-04-07 20:54:06 -0400

    Fixed error found by fuzzer

--- src/utils.rs
+++ src/utils.rs
@@ -1,14 +1,28 @@
 use std::ops::Bound;
-
 use super::Error;
 
-pub(crate) fn check_bounds(start_bound: Bound<&i64>, end_bound: Bound<&i64>) -> Result<(), Error> {
-    match (start_bound, end_bound) {
-        (Bound::Included(s), Bound::Included(e)) if s < e => Err(Error::BadRange),
-        (Bound::Included(s), Bound::Excluded(e)) if s <= e => Err(Error::BadRange),
-        (Bound::Excluded(s), Bound::Included(e)) if s <= e => Err(Error::BadRange),
-        _ => Ok(()),
+fn unwrap_bound(bound: Bound<&i64>) -> Option<i64> {
+    match bound {
+        Bound::Included(ts) => Some(*ts),
+        Bound::Excluded(ts) => Some(*ts),
+        _ => None
     }
+}
+
+pub(crate) fn check_bounds(start_bound: Bound<&i64>, end_bound: Bound<&i64>) -> Result<(), Error> {
+    if let (Some(start_ts), Some(end_ts)) = (unwrap_bound(start_bound), unwrap_bound(end_bound)) {
+        if end_ts < start_ts {
+            return Err(Error::BadRange);
+        } else if end_ts == start_ts {
+            if let Bound::Excluded(_) = end_bound {
+                return Err(Error::BadRange);
+            } else if let Bound::Excluded(_) = start_bound {
+                return Err(Error::BadRange);
+            }
+        }
+    }
+
+    Ok(())
 }
 
 #[cfg(test)]
